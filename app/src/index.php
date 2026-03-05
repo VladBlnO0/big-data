b729eb94d3b5fadf21671dbb6f84cded6a059b65
VlBl <v95506442@gmail.com>
2026-03-05 17:13:20 +0000

    curl and pdo

--- app/src/index.php
+++ app/src/index.php
@@ -1,38 +1,81 @@
-<?php
-// declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <title>Movie Data</title>
+</head>
+
+<body>
+    <h1>Movie Data</h1>
+</body>
+
+</html>
+<?php
+
+$json_string = file_get_contents('data.json');
+$data = json_decode($json_string, true);
+
+/** @var string $host host */
 $host = getenv('POSTGRES_HOST') ?: 'postgres-db';
+/** @var string $port port */
 $port = getenv('POSTGRES_PORT') ?: '5432';
+/** @var string $dbname dbname */
 $dbname = getenv('POSTGRES_DB');
+/** @var string $user user */
 $user = getenv('POSTGRES_USER');
+/** @var string $password password */
 $password = getenv('POSTGRES_PASSWORD');
 
-// Construct the DSN (Data Source Name)
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
 try {
-
-    // Create a new PDO instance
+    /** @var string $port PDO instance*/
     $pdo = new PDO($dsn, $user, $password);
-
-    // Set PDO attributes (optional, but recommended for error handling)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connected to the PostgreSQL database successfully with PDO!";
+    echo "Connected to the PostgreSQL";
 
-    // Example query using prepared statements to prevent SQL injection
-    $stmt = $pdo->prepare("SELECT * FROM coolscema.cooldata");
+    $sql_create = "CREATE TABLE IF NOT EXISTS movies (
+        imdb_id VARCHAR(50) PRIMARY KEY,
+        title TEXT,
+        release_year INTEGER,
+        rating DECIMAL(3,1),
+        description TEXT    
+    )";
+    $stmt_create = $pdo->prepare($sql_create);
+    $stmt_create->execute();
+    echo "Table 'movies' created successfully!<br>";
 
+    foreach ($data as $movie) {
+        $stmt = $pdo->prepare('
+        INSERT INTO movies (imdb_id, title, release_year, rating, description)
+        VALUES (:id, :title, :year, :rating, :desc)
+        ON CONFLICT (imdb_id) DO NOTHING
+    ');
+        $stmt->execute([
+            'id' => $movie['id'],
+            'title' => $movie['primaryTitle'],
+            'year' => $movie['startYear'],
+            'rating' => $movie['averageRating'],
+            'desc' => $movie['description']
+        ]);
+    }
+
+
+    echo "<pre>";
+    $stmt = $pdo->prepare("SELECT * FROM movies LIMIT 5");
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo "<pre>";
     print_r($result);
     echo "</pre>";
 
-    // Process the results...
-
+    echo "<pre>";
+    $stmt = $pdo->prepare("SELECT * FROM movies WHERE movies.rating >= 9");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($result);
+    echo "</pre>";
 } catch (PDOException $e) {
-    // Handle connection errors
     echo "Connection failed: " . $e->getMessage();
 }
+?>
